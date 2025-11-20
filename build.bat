50d2bc136ef39af1a070a2a6602dfab81b45bad9
202217b3231 <manshu3220@gmail.com>
2025-11-20 16:01:31 +0530

    fix cmake

--- build.bat
+++ build.bat
@@ -1,41 +1,54 @@
 @echo off
-set cfg=%1
-if "%cfg%"=="" goto debug
-if /I "%cfg%"=="release" goto release
-echo Invalid argument: %cfg%
-exit /b 1
+setlocal enabledelayedexpansion
 
-:debug
-call "C:\dev\msvc\VC\Auxiliary\Build\vcvars64.bat"
-if not exist debug mkdir debug
-cd debug
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
-cd ..
-exit /b
+if "%1"=="" (
+    call "C:\dev\msvc\VC\Auxiliary\Build\vcvarsall.bat" x64
+    if not exist debug mkdir debug
+    pushd debug
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug ..
+    cmake --build .
+    popd
+    echo Debug build completed
+    goto :eof
+)
 
-:release
-REM ---- Win32 ----
-call "C:\dev\msvc\VC\Auxiliary\Build\vcvars32.bat"
-if not exist build-win32 mkdir build-win32
-cd build-win32
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
-cd ..
+if "%1"=="release" (
+    goto :BUILD_RELEASE
+)
 
-REM ---- x64 ----
-call "C:\dev\msvc\VC\Auxiliary\Build\vcvars64.bat"
-if not exist build-x64 mkdir build-x64
-cd build-x64
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
-cd ..
+echo Usage: build.bat [release]
+goto :eof
 
-REM ---- ARM64 ----
-call "C:\dev\msvc\VC\Auxiliary\Build\vcvarsarm64.bat"
-if not exist build-arm64 mkdir build-arm64
-cd build-arm64
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+
+:BUILD_RELEASE
+echo Cleaning distribution folders...
+for %%A in (NppVim NppVim.x64 NppVim.arm64) do (
+    if exist "%%A" rmdir /s /q "%%A"
+)
+
+echo Building x64 Release...
+call "C:\dev\msvc\VC\Auxiliary\Build\vcvarsall.bat" x64
+if not exist build\x64 mkdir build\x64
+pushd build\x64
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..\..
 cmake --build .
-cd ..
-exit /b
+popd
+
+echo Building win32 Release...
+call "C:\dev\msvc\VC\Auxiliary\Build\vcvarsall.bat" x86
+if not exist build\win32 mkdir build\win32
+pushd build\win32
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..\..
+cmake --build .
+popd
+
+echo Building arm64 Release...
+call "C:\dev\msvc\VC\Auxiliary\Build\vcvarsall.bat" x64_arm64
+if not exist build\arm64 mkdir build\arm64
+pushd build\arm64
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..\..
+cmake --build .
+popd
+
+echo Release build complete.
+goto :eof
